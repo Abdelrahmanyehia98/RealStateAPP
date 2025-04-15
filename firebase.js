f6dbc7539fd90cc6584682da94274406a961b952
Abdelrahman yehia <abdelrahmanyahya98@gmail.com>
2025-04-15 13:33:04 +0200

    add firebase file for backend

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYjToFuVDbnkbEVBHyUPruOzwOgtaeow8",
+  authDomain: "app-project-fef04.firebaseapp.com",
+  projectId: "app-project-fef04",
+  storageBucket: "app-project-fef04.firebasestorage.app",
+  messagingSenderId: "203298090968",
+  appId: "1:203298090968:web:acb67d442bcfab9441ccad",
+  measurementId: "G-JHBN8R9PZ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
